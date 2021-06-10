9c63dd087d3636cfa477cded4ee676cdc12723d9
samj0516 <samj0516@gmail.com>
2021-06-10 16:23:36 -0500

    almost done with fixtures

--- dysclozur_api/models/post.py
+++ dysclozur_api/models/post.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     user = models.ForeignKey("DysclozurUser", on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(default=timezone.now, editable=False)
+    date_posted = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=300)
     text = models.TextField(null=True)
     link = models.URLField( null=True)

4cb9601d49ca8e70c84d3e35c711e6038ff2016c
samj0516 <samj0516@gmail.com>
2021-06-22 15:58:48 -0500

    videos are uploading properly, updating posts works with cloudinary

--- dysclozur_api/views/post.py
+++ dysclozur_api/views/post.py
@@ -52,12 +52,15 @@ class PostView(ViewSet):
         post.link = request.data['link']
         post.url_pic = request.data['url_pic']
         post.url_video = request.data['url_video']
-        upload_pic =  cloudinary.uploader.upload(request.data['upload_pic'], folder='dysclozurImages')
-        post.upload_pic = upload_pic['url']
-        if post.upload_video == None:
-            post.upload_video = request.data['upload_video']
+        if request.data['upload_pic'] == None:
+            post.upload_pic = None
         else:
-            upload_video = cloudinary.uploader.upload(request.data['upload_video'], folder='dysclozurVideo')
+            upload_pic =  cloudinary.uploader.upload(request.data['upload_pic'], folder='dysclozurImages', format='jpg')
+            post.upload_pic = upload_pic['url']
+        if request.data['upload_video'] == None:
+            post.upload_video = None
+        else:
+            upload_video = cloudinary.uploader.upload(request.data['upload_video'], folder='dysclozurVideo', resource_type = 'video', quality = "auto")
             post.upload_video = upload_video['url']
         
 
@@ -85,6 +88,10 @@ class PostView(ViewSet):
         Returns:
             Response -- Empty body with 204 status code
         """
+        cloudinary.config(cloud_name=settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+                  api_key=int(settings.CLOUDINARY_STORAGE['API_KEY']),
+                  api_secret=settings.CLOUDINARY_STORAGE['API_SECRET'])
+                  
         user = DysclozurUser.objects.get(user=request.auth.user)
         post = Post.objects.get(pk=pk)
         post.user = user
@@ -94,8 +101,16 @@ class PostView(ViewSet):
         post.link = request.data['link']
         post.url_pic = request.data['url_pic']
         post.url_video = request.data['url_video']
-        post.upload_pic = request.data["upload_pic"]
-        post.upload_video = request.data['upload_video']
+        if request.data['upload_pic'] == None:
+            post.upload_pic = None
+        else:
+            upload_pic =  cloudinary.uploader.upload(request.data['upload_pic'], folder='dysclozurImages', format='jpg')
+            post.upload_pic = upload_pic['url']
+        if request.data['upload_video'] == None:
+            post.upload_video = None
+        else:
+            upload_video = cloudinary.uploader.upload(request.data['upload_video'], folder='dysclozurVideo', resource_type = 'video', quality = "auto")
+            post.upload_video = upload_video['url']
         
         post.save()
         return Response({}, status=status.HTTP_204_NO_CONTENT)
